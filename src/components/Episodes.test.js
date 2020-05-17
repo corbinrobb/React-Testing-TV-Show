b48e0591a960048efccd482cec79deb5849ab1ee
Corbin Robb <corbinrobb@gmail.com>
2020-05-16 18:43:28 -0600

    Add tests for app

---
+++ src/components/Episodes.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from "@testing-library/react";
+import Episodes from './Episodes';
+import { appTestData } from '../AppTestData';
+
+
+test("Renders episode cards", async () => {
+  const { findAllByTestId } = render(<Episodes episodes={appTestData.data._embedded.episodes} />)
+
+  const episodes = await findAllByTestId(/episode/i);
+  expect(episodes).toHaveLength(8);
+})
